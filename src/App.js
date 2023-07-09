4d68e6f21f4aafbc68d5bfaac843b8146feba9f2
al-maruf <aalmaruf786@gmail.com>
2023-07-09 23:28:52 +0600

    create shining-car project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-secondary">Button</button>
+    </div>
+  );
+}
+
+export default App;
